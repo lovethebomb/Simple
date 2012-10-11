85ce3dffb26e1b4ab915e365e941a941d77118f3
Lucas <lucas@lovethebomb.eu>
2012-10-11 15:32:52 +0200

    First commit

--- Simple/ViewController.h
+++ Simple/ViewController.h
@@ -9,5 +9,16 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (retain, nonatomic) IBOutlet UILabel *helloLabel;
+@property (retain, nonatomic) IBOutlet UIButton *toggleLanguageButton;
+@property (retain, nonatomic) IBOutlet UISwitch *visibilitySwitch;
+@property (retain, nonatomic) IBOutlet UIImageView *imageView;
+@property (retain, nonatomic) IBOutlet UISegmentedControl *imageSelector;
+@property (retain, nonatomic) IBOutlet UISlider *opacitySlider;
+
+- (IBAction)toggleLanguage:(id)sender;
+- (IBAction)toggleVisible:(id)sender;
+- (IBAction)changeImage:(id)sender;
+- (IBAction)changeTransparency:(id)sender;
 
 @end
--- Simple/ViewController.m
+++ Simple/ViewController.m
@@ -13,6 +13,12 @@
 @end
 
 @implementation ViewController
+@synthesize helloLabel;
+@synthesize toggleLanguageButton;
+@synthesize visibilitySwitch;
+@synthesize imageView;
+@synthesize imageSelector;
+@synthesize opacitySlider;
 
 - (void)viewDidLoad
 {
@@ -22,6 +28,13 @@
 
 - (void)viewDidUnload
 {
+    [self setHelloLabel:nil];
+    [self setToggleLanguageButton:nil];
+    [self setVisibilitySwitch:nil];
+    [self setImageView:nil];
+    [self setImageSelector:nil];
+    [self setOpacitySlider:nil];
+    [self setHelloLabel:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
@@ -31,4 +44,58 @@
     return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
 }
 
+- (void)dealloc {
+    [helloLabel release];
+    [toggleLanguageButton release];
+    [visibilitySwitch release];
+    [imageView release];
+    [imageSelector release];
+    [opacitySlider release];
+    [helloLabel release];
+    [super dealloc];
+}
+- (IBAction)toggleLanguage:(id)sender {
+    if ([helloLabel.text isEqualToString:@"Hello world!"]) {
+        helloLabel.text = @"Bonjour le monde!";
+    } else {
+        helloLabel.text = @"Hello world!";
+    }
+
+}
+
+- (IBAction)toggleVisible:(id)sender {
+    self.helloLabel.hidden = !self.helloLabel.hidden;
+}
+
+- (IBAction)changeImage:(id)sender {
+    switch (imageSelector.selectedSegmentIndex) {
+        case 0:
+            imageView.image = [UIImage imageNamed:@"arcenciel.png"];
+            break;
+        case 1:
+            imageView.image = [UIImage imageNamed:@"mousse.png"];
+            break;
+        case 2:
+            imageView.image = [UIImage imageNamed:@"pirate.png"];
+            break;
+        case 3:
+            imageView.image = [UIImage imageNamed:@"plage.png"];
+            break;
+        case 4:
+            imageView.image = [UIImage imageNamed:@"sable.png"];
+            break;
+        case 5:
+            imageView.image = [UIImage imageNamed:@"pirate-cat.jpg"];
+            NSLog(@"Ahoy! Imeh a P'rat Cat young boy !");
+//            imageView.backgroundColor = [UIColor redColor];
+            break;
+        default: // defensive coding!
+            break;
+    }
+}
+
+- (IBAction)changeTransparency:(id)sender {
+    self.imageView.alpha = self.opacitySlider.value;
+    NSLog(@"changeTransparency : %f", self.opacitySlider.value);
+}
 @end
