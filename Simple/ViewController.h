6075db975039bf146f59000aff1851790ec57e38
HEYMES Lucas <lheymes@p-204-04.intra.gobelins.fr>
2012-10-11 09:36:57 +0200

    Initial Commit

---
+++ Simple/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Simple
+//
+//  Created by HEYMES Lucas on 11/10/12.
+//  Copyright (c) 2012 HEYMES Lucas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Simple/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Simple
+//
+//  Created by HEYMES Lucas on 11/10/12.
+//  Copyright (c) 2012 HEYMES Lucas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
